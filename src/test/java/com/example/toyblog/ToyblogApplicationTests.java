645b054460c7a1615e80799a11a1f6799f8c9b42
taewoocode <qkrxodn6035@gmail.com>
2024-03-24 16:22:34 +0900

    fisrt commit

---
+++ src/test/java/com/example/toyblog/ToyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
